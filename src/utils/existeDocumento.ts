848cbc27d489a5d0a9d3081d4c1b3e1c53905d10
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-09-27 20:25:18 -0300

    feat: Implementação da função que verifica se um documento existe

---
+++ src/utils/existeDocumento.ts
@@ -0,0 +1,12 @@
+import Cliente from "../ts/modelos/cliente";
+
+export default function ExisteDocumento(clientes: Cliente[], numero: string): boolean {
+    for (let cliente of clientes) {
+        for (let documento of cliente.Documentos) {
+            if (documento.Numero === numero) {
+                return true
+            }
+        }
+    }
+    return false
+}
