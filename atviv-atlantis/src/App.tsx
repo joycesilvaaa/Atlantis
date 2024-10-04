224ede898ef7924ccc74d46271b67201826c730a
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-10-04 16:55:41 -0300

    feat: Adicionado react-router-dom

--- atviv-atlantis/src/App.tsx
+++ atviv-atlantis/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Olá Mundo</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   );
 }
 
